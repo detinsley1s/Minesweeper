6f5817d1a685698cb07130a11524bbeb3e85e59a
Daniel Tinsley <detinsley1s@gmail.com>
2016-08-11 18:13:53 -0500

    Resized window to 800x600 and made it white

--- minesweeper.py
+++ minesweeper.py
@@ -16,8 +16,9 @@ class Room(sge.dsp.Room):
     pass
 
 
-Game(window_text='Minesweeper by Dan Tinsley', collision_events_enabled=False)
+Game(width=800, height=600, window_text='Minesweeper by Dan Tinsley', collision_events_enabled=False)
 
-sge.game.start_room = Room()
+background = sge.gfx.Background([], sge.gfx.Color("white"))
+sge.game.start_room = Room(background=background)
 
 sge.game.start()
