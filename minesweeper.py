d9ecf2ea87f63f19af100e3ad00ee519f67cd92b
Daniel Tinsley <detinsley1s@gmail.com>
2016-08-12 02:48:18 -0500

    Grid can detect when the mouse is within it.

--- minesweeper.py
+++ minesweeper.py
@@ -8,7 +8,11 @@ GRID_WIDTH = 20
 GRID_HEIGHT = 20
 TILE_SIDE_DIM = WINDOW_HEIGHT // GRID_HEIGHT
 
+# determines how many mines to have hidden
 mines_left = int(GRID_WIDTH * GRID_HEIGHT * 0.15)
+
+# for keeping track of the status of each individual cell
+cell_status = {'unclicked': 0, 'clicked': 1, 'flagged': 2}
 
 
 class Game(sge.dsp.Game):
@@ -30,6 +34,14 @@ class Room(sge.dsp.Room):
             color=sge.gfx.Color("black"), halign="center", valign="middle")
         sge.game.project_text(mines_left_font, str(mines_left), WINDOW_WIDTH - 100, 250,
             color=sge.gfx.Color("black"), halign="center", valign="middle")
+        print(int(sge.mouse.get_x() // TILE_SIDE_DIM))
+        print(int(sge.mouse.get_y() // TILE_SIDE_DIM))
+        if(0 <= int(sge.mouse.get_x() // TILE_SIDE_DIM) < GRID_WIDTH and
+            0 <= int(sge.mouse.get_y() // TILE_SIDE_DIM) < GRID_HEIGHT):
+            print('inside')
+        else:
+            print('outside')
+         
 
 
 class Tile(sge.dsp.Object):
@@ -50,7 +62,7 @@ def generate_hidden_cells():
     """Makes the board, and places bombs, along with their hint numbers"""
     mine_board = []
     for i in range(GRID_HEIGHT):
-        mine_board.append([0]*20)
+        mine_board.append([0]*GRID_WIDTH)
 
     # Place the bombs in the grid
     total_mines_to_place = mines_left
@@ -83,14 +95,12 @@ def generate_hidden_cells():
                    mine_board[i][j-1] += 1
                if j + 1 <= GRID_WIDTH - 1 and mine_board[i][j+1] != 'M':
                    mine_board[i][j+1] += 1
-    print(mine_board)
+    #print(mine_board)
     return mine_board
 
 
-Game(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, window_text='Minesweeper by Dan Tinsley',
+Game(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, window_text='Minesweeper by Dan Tinsley', grab_input=True,
     collision_events_enabled=False)
-
-
 
 tile_sprite = sge.gfx.Sprite(width=TILE_SIDE_DIM, height=TILE_SIDE_DIM, origin_x=0, origin_y=0)
 tile_sprite.draw_rectangle(0, 0, tile_sprite.width, tile_sprite.height, outline=sge.gfx.Color("black"),
@@ -104,9 +114,10 @@ tiles = generate_tiles()
 # Make initial hidden state of board
 mine_board = generate_hidden_cells()
 
-# Make every cell hidden
-board_cell_status = [[False] * GRID_WIDTH] * GRID_HEIGHT
-            
+# Make every cell hidden and never clicked
+board_cell_statuses = []
+for i in range(GRID_HEIGHT):
+    board_cell_statuses.append([cell_status['unclicked']]*GRID_WIDTH)
 
 objects = [*tiles]
 
@@ -115,5 +126,8 @@ mines_left_font = sge.gfx.Font(size=60)
 
 
 sge.game.start_room = Room(objects, background=background)
+
+sge.game.mouse.visible = True
+
 
 sge.game.start()
