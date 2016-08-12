f85c74989fb737721558a69e045b20f3f2093f48
Daniel Tinsley <detinsley1s@gmail.com>
2016-08-11 20:37:28 -0500

    Ability to draw complete grid

--- minesweeper.py
+++ minesweeper.py
@@ -4,8 +4,8 @@ import sge
 WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 800
 GRID_WIDTH = 20
-GRID_LENGTH = 20
-TILE_SIDE_DIM = 3 * WINDOW_WIDTH // (4 * GRID_WIDTH)
+GRID_HEIGHT = 20
+TILE_SIDE_DIM = WINDOW_HEIGHT // GRID_HEIGHT
 
 class Game(sge.dsp.Game):
 
@@ -28,17 +28,24 @@ class Tile(sge.dsp.Object):
         super().__init__(x, y, sprite=tile_sprite)
 
 
+def generate_tiles():
+    table = []
+    for i in range(GRID_WIDTH):
+        for j in range(GRID_HEIGHT):
+            table.append(Tile(i*TILE_SIDE_DIM, j*TILE_SIDE_DIM))
+    return table
+
+
 Game(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, window_text='Minesweeper by Dan Tinsley',
     collision_events_enabled=False)
 
-tile_sprite = sge.gfx.Sprite(width=TILE_SIDE_DIM, height=TILE_SIDE_DIM, origin_x=TILE_SIDE_DIM // 2,
-    origin_y=TILE_SIDE_DIM // 2)
+tile_sprite = sge.gfx.Sprite(width=TILE_SIDE_DIM, height=TILE_SIDE_DIM, origin_x=0, origin_y=0)
 tile_sprite.draw_rectangle(0, 0, tile_sprite.width, tile_sprite.height, outline=sge.gfx.Color("black"))
 
 background = sge.gfx.Background([], sge.gfx.Color("white"))
 
-tile = Tile(1, 1)
-objects = [tile]
+tiles = generate_tiles()
+objects = [*tiles]
 
 sge.game.start_room = sge.dsp.Room(objects, background=background)
 
